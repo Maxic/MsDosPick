69aa1b2e4b41ae4e8c622110ae3bef35093436a0
Barend <barend.poot@gmail.com>
2018-08-01 18:09:31 +0200

    ReadMe + some fixes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MsDosPick
 Small program to help us pick a random DOS game from the MS-DOS software library
+
+# Usage:
+
+- Install all requirements with pip
+- Run ia configure and login with your internet archive account
+    - If you down have an account, create one here: TODO LINK
+- Run pick_game
+
+# Notes:
+
+IA is not required, if you do not have it configured or don't have internet, the script uses the included JSON file (msdoslib.json) to pick a title.
--- pick_game.py
+++ pick_game.py
@@ -3,21 +3,34 @@ import random
 import os
 from internetarchive import search_items
 
-try:
-    titles = search_items("collection:(softwarelibrary_msdos_games)", ["title"])
-except Exception as e:
-    print("Could not get most recent list of titles.")
-    pass
 
-if not titles:
-    path_dir = os.path.dirname(os.path.realpath(__file__))
-    file_dir = path_dir + "\msdoslib.json"
-    with open(file_dir) as file:
-        file_content = file.read().rstrip("\n")
-        json_object = json.loads(file_content)
-    titles = json_object['response']['docs']
+def main():
+    chosen_game = None
+    games_list_file = "msdoslib.json"
+    games_done_file = "done.txt"
 
-chosen_game = str(random.choice(titles)) + ",\n"
+    try:
+        titles = search_items("collection:(softwarelibrary_msdos_games)", ["title"])
+        chosen_game = str(random.choice(titles)) + ",\n"
+    except Exception as e:
+        print("Could not get most recent list of titles.")
+        print(e)
+        pass
 
-with open(path_dir + "/done.txt", 'a+') as file:
-    file.write(str(chosen_game))
+    if chosen_game is None:
+        path_dir = os.path.dirname(os.path.realpath(__file__))
+        file_dir = os.path.join(path_dir, games_list_file)
+        with open(file_dir) as file:
+            file_content = file.read().rstrip("\n")
+            json_object = json.loads(file_content)
+        titles = json_object['response']['docs']
+        chosen_game = str(random.choice(titles)) + ",\n"
+
+    file_path = os.path.join(path_dir,games_done_file)
+    with open(file_path, 'a+') as file:
+        file.write(str(chosen_game))
+    print("Added new title to" + games_done_file)
+
+
+if __name__ == "__main__":
+    main()
