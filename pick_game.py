fbd4fd3e35c4234af264365fbebd8610e08d2c1e
Barend <barend.poot@gmail.com>
2018-10-15 23:34:31 +0200

    lots of changes

--- pick_game.py
+++ pick_game.py
@@ -1,60 +1,44 @@
 import json
 import random
-import os
 from internetarchive import search_items
 
 
-class GamePicker:
-    def old(self):
-        games_list_file = "msdoslib.json"
-        games_done_file = "done.txt"
-        path_dir = os.path.dirname(os.path.realpath(__file__))
+def main():
+    games_done_file = "done.json"
 
-        titles = search_items("collection:(softwarelibrary_msdos_games)", ["title", "year"])
-
-        chosen_game1 = self.pick_game(games_list_file, path_dir, titles)
-        chosen_game2 = self.pick_game(games_list_file, path_dir, titles)
-        chosen_game3 = self.pick_game(games_list_file, path_dir, titles)
-
-        file_path = os.path.join(path_dir, games_done_file)
-
-        with open(file_path, 'a+') as file:
-            file.write(str(chosen_game1))
-            file.write(str(chosen_game2))
-            file.write(str(chosen_game3) + "\n")
-        print("Added new title to " + games_done_file)
-
-    def get_titles_from_archive(self):
-        titles = search_items("collection:(softwarelibrary_msdos_games)", ["title","year"])
-        return titles
-
-    def pick_game(self, titles):
-        try:
-            chosen_game = str(random.choice(list(titles))) + ",\n"
-        except Exception as e:
-            print("Could not get most recent list of titles.")
-            print(e)
-            return None
-
-        return chosen_game
-
-    def pick_game_old(self, games_list_file, path_dir, titles):
-        chosen_game = None
-
-        try:
-            chosen_game = str(random.choice(list(titles))) + ",\n"
-        except Exception as e:
-            print("Could not get most recent list of titles.")
-            print(e)
-            pass
-        if chosen_game is None:
-            file_dir = os.path.join(path_dir, games_list_file)
-            with open(file_dir) as file:
-                file_content = file.read().rstrip("\n")
-                json_object = json.loads(file_content)
-            titles = json_object['response']['docs']
-            chosen_game = str(random.choice(titles)) + ",\n"
-
-        return chosen_game
+    with open(games_done_file, "r+") as file:
+        games_done_data = json.load(file)
+        chosen_games = pick_game(3, games_done_data)
+        for game in chosen_games:
+            game['link'] = "https://archive.org/details/" + game['identifier']
+            del game['identifier']
+            games_done_data['games'].append(game)
+        file.seek(0)
+        file.write(json.dumps(games_done_data))
 
 
+def is_duplicate(game_done_data, new_game):
+    for game in game_done_data['games']:
+        if game['title'] == new_game['title']:
+            return True
+    return False
+
+
+def pick_game(game_amount, game_done_data):
+    chosen_games = []
+
+    try:
+        titles = list(search_items("collection:(softwarelibrary_msdos_games)", ["title", "identifier"]))
+        for _ in range(game_amount):
+            new_game = random.choice(titles)
+            while is_duplicate(game_done_data, new_game):
+                new_game = random.choice(titles)
+            chosen_games.append(new_game)
+    except Exception as e:
+        print("Could not get most recent list of titles.")
+        print(e)
+    return chosen_games
+
+
+if __name__ == "__main__":
+    main()
